bf6784c7f8b409a44bc46c7ccc075a0c47cbe3d9
Jill Short <jillian.b.short@gmail.com>
2020-07-16 19:32:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,4 @@
 #require 'pry'
 def prime?(number)
-  numbers = []
-   numbers.each do |n|
-     n / numbers<n
-   if any? numbers.each = numbers
-     return true
-   else
-     return false
-   end
- end
+  
 end
