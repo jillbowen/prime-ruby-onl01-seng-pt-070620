f966d4ae35b85ccdaec1143bdbfeffd4103d746d
Jill Short <jillian.b.short@gmail.com>
2020-07-16 19:47:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 def prime?(number)
   if number <= 1  
     return false
+  elsif number / number<number = number
+    return true
   elsif number = 4
     return false
-  else number / number<number = number
-    return true
   end
 end
